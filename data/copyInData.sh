d614f84b1c6143683db752969b8b90aff382f79d
Jonathan Olson <jonathan.olson@colorado.edu>
2022-01-18 01:19:54 -0700

    Updated data

--- data/copyInData.sh
+++ data/copyInData.sh
@@ -1,4 +1,7 @@
 
+echo -n "export default " > crane.tree.total.js
+cat ../../miscworks/computed/wordle/crane.tree.total.json >> crane.tree.total.js
+
 echo -n "export default " > crate.tree.total.js
 cat ../../miscworks/computed/wordle/crate.tree.total.json >> crate.tree.total.js
 
@@ -10,6 +13,9 @@ cat ../../miscworks/computed/wordle/trace.tree.total.json >> trace.tree.total.js
 
 echo -n "export default " > alter.tree.js
 cat ../../miscworks/computed/wordle/alter.tree.json >> alter.tree.js
+
+echo -n "export default " > lance.tree.js
+cat ../../miscworks/computed/wordle/lance.tree.json >> lance.tree.js
 
 echo -n "export default " > rance.tree.js
 cat ../../miscworks/computed/wordle/rance.tree.json >> rance.tree.js
