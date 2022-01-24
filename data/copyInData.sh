21eca67e2809aedc00f46920a62aa49333db5b78
Jonathan Olson <jonathan.olson@colorado.edu>
2022-01-24 11:57:47 -0700

    Wordle updates

--- data/copyInData.sh
+++ data/copyInData.sh
@@ -11,6 +11,12 @@ cat ../../miscworks/computed/wordle/slate.tree.total.json >> slate.tree.total.js
 echo -n "export default " > trace.tree.total.js
 cat ../../miscworks/computed/wordle/trace.tree.total.json >> trace.tree.total.js
 
+echo -n "export default " > salet.tree.total.js
+cat ../../miscworks/computed/wordle/salet.tree.total.json >> salet.tree.total.js
+
+echo -n "export default " > reast.tree.total.js
+cat ../../miscworks/computed/wordle/reast.tree.total.json >> reast.tree.total.js
+
 echo -n "export default " > alter.tree.js
 cat ../../miscworks/computed/wordle/alter.tree.json >> alter.tree.js
 
@@ -22,4 +28,10 @@ cat ../../miscworks/computed/wordle/rance.tree.json >> rance.tree.js
 
 echo -n "export default " > rated.tree.js
 cat ../../miscworks/computed/wordle/rated.tree.json >> rated.tree.js
+
+echo -n "export default " > rants.tree.js
+cat ../../miscworks/computed/wordle/rants.tree.json >> rants.tree.js
+
+echo -n "export default " > ronte.tree.js
+cat ../../miscworks/computed/wordle/ronte.tree.json >> ronte.tree.js
 
